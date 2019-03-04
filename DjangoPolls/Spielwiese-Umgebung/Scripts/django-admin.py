712862ffc0a13d9147dd26afbefcf7955fe8492f
ductungnguyen <ductung.nguyen@sblum.de>
2019-03-04 18:29:15 +0100

    Erstellung neues Frag-ein-Django-Web-ab (in Bearbeitung)

---
+++ DjangoPolls/Spielwiese-Umgebung/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\tungnguyen\source\repos\PythonSpielwiese\DjangoPolls\Spielwiese-Umgebung\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
